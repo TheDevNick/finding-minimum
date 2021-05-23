4d72950cf171c02f864d95228dddd4a566e529b1
Nick Hollins <nholl22@wgu.edu>
2021-05-23 09:31:22 -0400

    finding the minimum coding challenge

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+/*
+Write a function min that takes two arguments and returns their minimum.
+*/
+
+function displayMin(a, b) {
+    return Math.min(a, b)
+}
+
+console.log(displayMin(0, 10));//0
+console.log(displayMin(0, -10));//-10
